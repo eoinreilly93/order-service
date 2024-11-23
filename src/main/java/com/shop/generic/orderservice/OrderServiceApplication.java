14d687dd31607c6a03880d5ca04aa90f44831265
eoinreilly <eoindreilly@gmail.com>
2024-11-23 16:47:10 +0000

    chore: Document why kafkaadmin bean is required
    - Could be in its own config file, but leaving here for time being

--- src/main/java/com/shop/generic/orderservice/OrderServiceApplication.java
+++ src/main/java/com/shop/generic/orderservice/OrderServiceApplication.java
@@ -4,6 +4,7 @@ import com.shop.generic.common.CommonKafkaConsumerAutoConfiguration;
 import java.util.Map;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,14 +14,22 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(exclude = {CommonKafkaConsumerAutoConfiguration.class})
 public class OrderServiceApplication {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     public static void main(final String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    /**
+     * Used as part of the
+     * {@link com.shop.generic.orderservice.actuator.endpoints.KafkaHealthIndicator} for spring boot
+     * admin
+     */
     @Bean
     public AdminClient adminClient() {
         return AdminClient.create(Map.of(
-                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers
         ));
     }
 }
